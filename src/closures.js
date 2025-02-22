1dd01bea75f866b36e0c9993ce4739c248b38ba3
ChelGame <chel.06081995@gmail.com>
2025-02-22 16:49:28 +0300

    add closured homework

---
+++ src/closures.js
@@ -0,0 +1,18 @@
+function createLogger() {
+    const logs = [];
+
+    return {
+        log(message) {
+            logs.push(message);
+        },
+        getLogs() {
+            return logs;
+        },
+    };
+}
+
+function createRandomGenerator(min, max) {
+    return function () {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+}
